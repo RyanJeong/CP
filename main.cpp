4192b8adef14f5ee21d09af165d78469eab75c57
RyanJeong <wat6988@gmail.com>
2021-06-17 21:15:00 +0900

    Updated on Jun 17, 2021

--- main.cpp
+++ main.cpp
@@ -3,83 +3,65 @@
 
 using namespace std;
 
+void dfs(int, int, int);
+
+int n, m;
+vector<int> answer;
+bool arr[101][101];
+bool is_visited[101][101];
+
 int main(void)
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    cin>>m>>n;
+    for (int i = 1; i<=n; ++i) {
+        string s;
+        cin>>s;
+
+        int j = 1;
+        for (char c : s) {
+            arr[i][j++]=c-'0';
+        }
+    }
+
+    is_visited[1][1]=1;
+    dfs(1,1,arr[1][1]);
+    sort(answer.begin(),answer.end());
+    cout << answer.front();
+
+    return 0;
+}
+
+void dfs(int i, int j, int cnt)
+{
 	// right, botq_tom, left, top
 	const int dx[4] = {1,0,-1,0};
 	const int dy[4] = {0,-1,0,1};
 
-	bool is_lighted[101][101]; 
-    memset(is_lighted,false,sizeof is_lighted);
-    bool is_visited[101][101];
-    memset(is_visited,false,sizeof is_visited);
-	queue<pair<int, int>> q;
-	q.push({1,1});
-    is_lighted[1][1]=true;
-    is_visited[1][1]=true;
-	int n, m;
-	cin>>n>>m;
-	vector<pair<int, int>> v[101][101];
-	while (m--) {
-		int x, y, a, b;
-		cin>>x>>y>>a>>b;
-        v[x][y].push_back({a,b});
-	}
+    if (i==n && j==m) {
+        answer.push_back(cnt);
 
-	while (!q.empty()) {
-        auto cur = q.front();
-        q.pop();
+        return;
+    }
+    for (int d = 0; d<4; ++d) {
+        int x = i+dx[d];
+        int y = j+dy[d];
 
-        for (auto to : v[cur.first][cur.second]) {
-            if (is_visited[to.first][to.second]) {
-                continue;
-            }
-            for (int d = 0; d<4; ++d) {
-                int x = to.first+dx[d];
-                int y = to.second+dy[d];
-
-                if (x<1 || x>n) {
-                    continue;
-                }
-                if (y<1 || y>n) {
-                    continue;
-                }
-                is_lighted[to.first][to.second]=true;
-                // The position 'to' is connected
-                if (!is_visited[to.first][to.second] && is_visited[x][y]) {
-                   is_visited[to.first][to.second]=1;
-                   q.push({to.first,to.second});
-                }
-            }
+        if (x<1 || x>n) {
+            continue;
         }
-        for (int d = 0; d<4; ++d) {
-            int x = cur.first+dx[d];
-            int y = cur.second+dy[d];
-
-            if (x<1 || x>n) {
-                continue;
-            }
-            if (y<1 || y>n) {
-                continue;
-            }
-            if (!is_lighted[x][y] || is_visited[x][y]) {
-                continue;
-            }
-            is_visited[x][y]=true;
-            q.push({x,y});
+        if (y<1 || y>m) {
+            continue;
         }
+        if (is_visited[x][y]) {
+            continue;
+        }
+        is_visited[x][y]=true;
+        dfs(x,y,cnt+arr[x][y]);
+        is_visited[x][y]=false;
     }
 
-    int room = 0;
-    for (int i = 1; i<=n; ++i) {
-        for (int j = 1; j<=n; ++j) {
-            room+=is_lighted[i][j];
-        }
-    }
-    cout << room;
-
-    return 0;
+    return;
 }
