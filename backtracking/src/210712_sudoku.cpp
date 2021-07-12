c17f2bcbcb44cefe23c9b388c374b799212aea06
RyanJeong <wat6988@gmail.com>
2021-07-12 22:10:21 +0900

    Updated on Jul 12, 2021

---
+++ backtracking/src/210712_sudoku.cpp
@@ -0,0 +1,82 @@
+// https://www.acmicpc.net/problem/2239
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[9][9];
+/*
+ 1 2 3 
+ 4 5 6 
+ 7 8 9
+*/
+bool is_used1[3][3][10]; // subsquare
+bool is_used2[9][10]; // row
+bool is_used3[9][10]; // col
+
+bool bt(int, int);
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	for (int i = 0; i<9; ++i) {
+		for (int j = 0; j<9; ++j) {
+			char c;
+			cin>>c;
+			arr[i][j]=c-'0';
+
+            // subsquare|row|col, digit
+            is_used1[i/3][j/3][arr[i][j]]=true;
+            is_used2[i][arr[i][j]]=true;
+            is_used3[j][arr[i][j]]=true;
+		}
+		cin.ignore();
+	}
+    bt(0,0);
+	for (int i = 0; i<9; ++i) {
+		for (int j = 0; j<9; ++j) {
+            cout << arr[i][j];
+        }
+        cout << '\n';
+    }
+
+	return 0;
+}
+
+bool bt(int i, int j)
+{
+    while (i<9) {
+        while (j<9) {
+            if (!arr[i][j]) {
+                for (int n = 1; n<=9; ++n) {
+                    if (is_used1[i/3][j/3][n]==false
+                      && is_used2[i][n]==false
+                      && is_used3[j][n]==false) {
+                        is_used1[i/3][j/3][n]=true;
+                        is_used2[i][n]=true;
+                        is_used3[j][n]=true;
+                        arr[i][j]=n;
+                        if (bt(i,j+1)) {
+
+                            return true;
+                        }
+                        is_used1[i/3][j/3][n]=false;
+                        is_used2[i][n]=false;
+                        is_used3[j][n]=false;
+                        arr[i][j]=0;
+                    }
+                }
+            }
+            if (!arr[i][j]) {
+
+                return false;
+            }
+            ++j;
+        }
+        j=0;
+        ++i;
+    }
+
+    return true;
+}
--- main.cpp
+++ main.cpp
@@ -1,32 +1,42 @@
-// https://www.acmicpc.net/problem/2239
+// https://www.acmicpc.net/problem/16920
+// 1. 1~n 순서대로 bfs
+// 2. bfs 카운트 배열에 기록
+// 3. 기록하면서 개수 카운트
+// 4. 1에 대해서 수행이 완료되면 2에 대해서 2~3 반복 
+// 4-1. 1이 기록한 배열에 2가 덮어쓰되, 1이 기록한 숫자 중 작은 수만 2가 새로 덮어씀
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int arr[9][9];
-int row_cond[9]; // sum by row is 45
-int col_cond[9]; // sum by col is 45
-int sqr_cond[3][3]; // sum by sqr
-
-void bt(int, int);
 
 int main(void)
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-	for (int i = 0; i<9; ++i) {
-		for (int j = 0; j<9; ++j) {
-			char c;
-			cin>>c;
-			arr[i][j]=c-'0';
-			row_cond[i]+=arr[i][j];
-			col_cond[j]+=arr[i][j];
-			sqr_cond[i/3][j/3]+=arr[i][j];
-		}
-		cin.ignore();
-	}
-	
+    static int arr[1001][1001];
+    static bool is_visited[1001][1001];
+    static int movement[10];
+    int n, m, p;
+    cin>>n>>m>>p;
+    for (int i = 1; i<=p; ++i) {
+        cin>>movement[i];
+    }
+    vector<pair<int, int>> v[p+1];
+    for (int i = 1; i<=n; ++i) {
+        for (int j = 1; j<=m; ++j) {
+            char c;
+            cin>>c;
+            if (isdigit(c)) {
+                v[c-'0'].push_back({i,j});
+            }
+        }
+        cin.ignore();
+    }
 
-	return 0;
+    // bfs
+    const int dx[] = {1,0,-1,0};
+    const int dy[] = {0,-1,0,1};
+    //for (int i = 1; i<=p; ++i) while (!v[i].empty())
+
+    return 0;
 }
