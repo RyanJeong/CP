9f167b6d27c1147172b63719459bb224d6299513
RyanJeong <wat6988@gmail.com>
2021-07-23 15:54:03 +0900

    Updated on Jul 23, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,6 +1,13 @@
+// https://www.acmicpc.net/problem/2630
 #include <bits/stdc++.h>
 
 using namespace std;
+
+void rec(int, int, int);
+
+int arr[128+1][128+1]; // 1-based
+int n_white;
+int n_blue;
 
 int main(void)
 {
@@ -9,23 +16,56 @@ int main(void)
 
     int n;
     cin>>n;
-    vector<int> v(n+1); // 1-based
     for (int i = 1; i<=n; ++i) {
-        cin>>v[i];
-    }
-
-    vector<int> lis;
-    lis.push_back(v[1]);
-    for (int i = 2; i<=n; ++i) {
-        if (v[i]>lis.back()) {
-            lis.push_back(v[i]);
-        }
-        else {
-            auto cur = lower_bound(lis.begin(),lis.end(),v[i]);
-            lis[cur-lis.begin()]=v[i];
+        for (int j = 1; j<=n; ++j) {
+            cin>>arr[i][j];
         }
     }
-    cout << n-lis.size();
+    rec(n,1,1);
+    cout << n_white << '\n' << n_blue;
 
     return 0;
+}
+
+void rec(int n, int x, int y)
+{
+    if (n==1) {
+        (arr[x][y]) ? ++n_blue : ++n_white;
+
+        return;
+    }
+    bool is_white = true;
+    bool is_blue = true;
+    for (int i = x; i<x+n; ++i) {
+        for (int j = y; j<y+n; ++j) {
+            if (arr[i][j]) {
+                
+            }
+        }
+    }
+    for (int i = x; i<x+n; ++i) {
+        for (int j = y; j<y+n; ++j) {
+            if (arr[i][j]) {
+                is_white=false;
+            } 
+            else {
+                is_blue=false;
+            }
+        }
+    }
+    if (is_white) {
+        ++n_white;
+    }
+    if (is_blue) {
+        ++n_blue;
+    }
+    else {
+        n/=2;
+        rec(n,x,y);
+        rec(n,x,y+n);
+        rec(n,x+n,y);
+        rec(n,x+n,y+n);
+    }
+
+    return;
 }
