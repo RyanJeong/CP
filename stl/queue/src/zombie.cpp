f26238876944b924a8e6c9052c64a8310d4f4120
RyanJeong <wat6988@gmail.com>
2021-07-26 01:33:59 +0900

    Updated on Jul 26, 2021

--- stl/queue/src/zombie.cpp
+++ stl/queue/src/zombie.cpp
@@ -12,6 +12,9 @@ int main(void)
 	bool is_survived = true;
 	queue<int> mine;
 	for (int i = 1; i<=l; ++i) {
+		// 만약 지뢰를 사용하게 된다면 
+		// 공격 가능한 범위에 속한 나머지 좀비들은
+		// 공격을 한 회 덜 받게 된다.
 		if (!mine.empty() && mine.back()<i) {
 			mine.pop();
 		}
@@ -25,10 +28,14 @@ int main(void)
 		}
 		if (hp>0) {
 			if (c>0) {
+				// 지뢰를 사용해 공격을 덜 받는
+				// 마지막 인덱스를 값으로 queue에 삽입
 				mine.push(i+r-1);
 				--c;
 			}
 			else {
+				// 지뢰를 사용해야 하는 상황에서
+				// 지뢰가 더 이상 없을 경우
 				is_survived=false;
 				break;
 			}
