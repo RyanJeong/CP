2e64b66402d2e1855680455d3d48c82aed45cb7c
RyanJeong <wat6988@gmail.com>
2021-09-14 19:36:16 +0900

    Updated on Sep 14, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,21 +7,23 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, m;
-    cin>>n>>m;
-
-    map<string, string> pw_of;
-    while (n--) {
-        string site, pw;
-        cin>>site>>pw;
-        pw_of[site]=pw;
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for (int i = 0; i<n; ++i) {
+        cin>>v[i];
     }
-
-    while (m--) {
-        string site;
-        cin>>site;
-        cout << pw_of[site] << '\n';
-    }
-
+    sort(v.begin(),v.end());
+    
+    int res = 0;
+    do {
+        int tmp = 0;
+        for (int i = 0; i<n-1; ++i) {
+            tmp+=abs(v[i]-v[i+1]);
+        }
+        res=max(res,tmp);
+    } while (next_permutation(v.begin(),v.end()));
+    cout << res;
+    
     return 0;
 }
