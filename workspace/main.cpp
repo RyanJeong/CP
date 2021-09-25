ce815da6838de5fcd6745d122b58aeeec12378b8
RyanJeong <wat6988@gmail.com>
2021-09-25 17:12:38 +0900

    Updated on Sep 25, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-// https://www.acmicpc.net/problem/1389
-=======
-// https://www.acmicpc.net/problem/2870
->>>>>>> cb7420272c8de0f67e8e23020cb8152542066050
+// https://www.acmicpc.net/problem/2458
 #include <bits/stdc++.h>
 
 using namespace std;
-
-bool cmp(string&, string&);
 
 int main(void) 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-<<<<<<< HEAD
 
     int n, m;
     cin>>n>>m;
@@ -26,87 +19,33 @@ int main(void)
         int a, b;
         cin>>a>>b;
         d[a][b]=1;
-        d[b][a]=1;
     }
 
     for (int k = 1; k<=n; ++k) {
         for (int i = 1; i<=n; ++i) {
             for (int j = 1; j<=n; ++j) {
                 d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
-=======
-
-	int n;
-    cin>>n;
-    
-    vector<string> res;
-    while (n--) {
-        string str;
-        cin>>str;
-
-        string tmp;
-        for (char& c : str) {
-            if (isdigit(c)) {
-                tmp+=c;
             }
-            else {
-                if (tmp.empty()) {
-                    continue;
-                }
-                while (tmp.front()=='0' && tmp.size()>1) {
-                    tmp.erase(0,1);
-                }
-                res.push_back(tmp);
-                tmp.clear();
-            }
-        }
-        if (!tmp.empty()) {
-            while (tmp.front()=='0' && tmp.size()>1) {
-                tmp.erase(0,1);
->>>>>>> cb7420272c8de0f67e8e23020cb8152542066050
-            }
-            res.push_back(tmp);
-            tmp.clear();
         }
     }
-<<<<<<< HEAD
-
-    int idx = 0;
-    int min_edge = 1e9;
+    int res = 0;
     for (int i = 1; i<=n; ++i) {
-        int tmp = 0;
+        bool is_succeed = true;
         for (int j = 1; j<=n; ++j) {
-            tmp+=d[i][j];
+            if (d[i][j]!=1e9) {
+                continue;
+            }
+            if (d[j][i]!=1e9) {
+                continue;
+            }
+            is_succeed=false;
+            break;
         }
-        if (min_edge>tmp) {
-            min_edge=tmp;
-            idx=i;
+        if (is_succeed) {
+            ++res;
         }
-=======
-    sort(res.begin(),res.end(),cmp);
-    for (string& str : res) {
-        cout << str << '\n';
     }
+    cout << res;
 
     return 0;
-}
-
-bool cmp(string& a, string& b) 
-{ 
-    if (a.size()!=b.size()) {
-
-        return a.size()<b.size();
-    }
-
-    int n = a.size();
-    for (int i = 0; i<n; ++i) {
-        if (a[i]==b[i]) {
-            continue;
-        }
-
-        return a[i]<b[i];
->>>>>>> cb7420272c8de0f67e8e23020cb8152542066050
-    }
-    cout << idx;
-
-    return true;
 }
