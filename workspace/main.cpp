19e4f897f769273db7c98bec1e68686816fd7b25
RyanJeong <wat6988@gmail.com>
2021-09-27 16:00:08 +0900

    Updated on Sep 27, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,4 +1,3 @@
-// https://www.acmicpc.net/problem/14588
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,48 +9,23 @@ int main(void)
 
     int n;
     cin>>n;
-    vector<pair<int, int>> v(n+1);
-    for (int i = 1; i<=n; ++i) {
-        cin>>v[i].first>>v[i].second;
+    vector<int> v(n);
+    int sum = 0;
+    for (int i = 0; i<n; ++i) {
+        cin>>v[i];
+        sum+=v[i];
     }
 
-    vector<vector<int>> d(n+1,vector<int>(n+1));
-    fill(d.begin(),d.end(),vector<int>(n+1,1e9));
-    for (int i = 1; i<=n; ++i) {
-        d[i][i]=0;
-    }
-    for (int i = 1; i<=n; ++i) {
-        for (int j = 1; j<=n; ++j) {
-            if (i==j) {
-                continue;
-            }
-
-            /* 
-                     -----      i
-                ---        ---- j
-            */
-            if (v[j].second<v[i].first || v[i].second<v[j].first) {
-                continue;
-            }
-            d[i][j]=1;
-            d[j][i]=1;
+    sum/=2;
+    bool is_used = false;
+    for (auto i : v) {
+        if (!is_used && i==sum) {
+            is_used=true;
+            continue;
         }
+        cout << i << ' ';
     }
-    for (int k = 1; k<=n; ++k) {
-        for (int i = 1; i<=n; ++i) {
-            for (int j = 1; j<=n; ++j) {
-                d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
-            }
-        }
-    }
-
-    int q;
-    cin>>q;
-    while (q--) {
-        int a, b;
-        cin>>a>>b;
-        cout << ((d[a][b]==1e9) ? -1 : d[a][b]) << '\n';
-    }
+    cout << sum;
 
     return 0;
 }
