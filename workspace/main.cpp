8dcc272b12daf21cddabe14d5783190f86e22f70
RyanJeong <wat6988@gmail.com>
2021-10-03 09:11:36 +0900

    Updated on Oct 3, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -2,46 +2,22 @@
 
 using namespace std;
 
-vector<int> get_fail(string);
-
 int main(void)
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    while (true) {
-        string s;
-        cin>>s;
-        if (s==".") {
-            break;
-        }
-
-        int len = s.size();
-        auto fail = get_fail(s);
-        if (len%(len-fail.back())) {
-            cout << 1 << '\n';
-        }
-        else {
-            cout << len/(len-fail.back()) << '\n';
-        }
+    while (true) { 
+        double weight; 
+        cin>>weight; 
+        
+        if (weight<0.0) { 
+            break; 
+        } 
+        cout << fixed;
+        cout.precision(2);
+        cout << "Objects weighing " << weight << " on Earth will weigh " << weight*0.167 << " on the moon." << '\n';
     }
 
     return 0;
-}
-
-vector<int> get_fail(string p)
-{
-    int n_p = p.size();
-    vector<int> fail(n_p);
-    for (int i = 1, j = 0; i<n_p; i++) {
-        while (j>0 && p[i]!=p[j]) {
-            j=fail[j-1];
-        }
-        if (p[i]==p[j]) {
-            fail[i]=j+1;
-            ++j;
-        }
-    }
-
-    return fail;
 }
