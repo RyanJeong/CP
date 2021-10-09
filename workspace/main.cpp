54a1a7f022053eabbf15bdac125783c1a97890f6
RyanJeong <wat6988@gmail.com>
2021-10-09 11:35:26 +0900

    Updated on Oct 9, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,4 +1,3 @@
-/*
 // https://www.acmicpc.net/problem/1107
 #include <bits/stdc++.h>
  
@@ -19,11 +18,26 @@ int main(void)
         is_broken[tmp]=true;
     }
 
-    int start = 100;
-    int res = abs(n-start);
+    int res = abs(n-100);
+    for (int i = 0; i<1'000'000; ++i) {
+        int cnt = 0;
+        int ch = i;
+        do {
+            if (is_broken[ch%10]) {
+                cnt=0;
+                break;
+            }
+            ++cnt;
+            ch/=10;
+        } while (ch>0);
+        
+        if (cnt>0) {
+            cnt+=abs(n-i);
+            res=min(res,cnt);
+        }
+    }
 
     cout << res;
 
     return 0;
 }
-*/
