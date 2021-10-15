c3d14b5371a923c1c897bc13694c30b4fc9ce004
RyanJeong <wat6988@gmail.com>
2021-10-15 11:58:24 +0900

    Updated on Oct 15, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,93 +7,95 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin>>n;
-    vector<vector<int>> v(n+1,vector<int>(n+1)); // 1-based
-    int s_x, s_y;
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int>> v(n+1,vector<int>(m+1)); // 1-based;
+    vector<pair<int, int>> empty_area;
+    int wall_size = 0;
     for (int i = 1; i<=n; ++i) {
-        for (int j = 1; j<=n; ++j) {
+        for (int j = 1; j<=m; ++j) {
             cin>>v[i][j];
-            if (v[i][j]==9) {
-                s_x=i;
-                s_y=j;
-                v[i][j]=0;
+            if (v[i][j]==0) {
+                empty_area.push_back({i,j});
+            }
+            else if (v[i][j]==1) {
+                ++wall_size;
             }
         }
     }
 
-    int cnt = 0;
-    int lv = 2;
-    int lv_cnt = lv;
-    vector<vector<int>> dist(n+1,vector<int>(n+1));
-    while (true) {
-        fill(dist.begin(),dist.end(),vector<int>(n+1,-1));
-        queue<pair<int, int>> q;
-        q.push({s_x,s_y});
-        dist[s_x][s_y]=0;
-        int d_x = -1;
-        int d_y = -1;
-        int min_dist = 1e9;
-        while (!q.empty()) {
-            auto cur = q.front();
-            q.pop();
+    int empty_size = empty_area.size();
+    int res = n*m;
+    vector<vector<bool>> is_visited(n+1,vector<bool>(m+1));
+    for (int first = 0; first<empty_size-2; ++first) {
+        int first_x = empty_area[first].first;
+        int first_y = empty_area[first].second;
+        v[first_x][first_y]=1;
+        for (int second = first+1; second<empty_size-1; ++second) {
+            int second_x = empty_area[second].first;
+            int second_y = empty_area[second].second;
+            v[second_x][second_y]=1;
+            for (int third = second+1; third<empty_size; ++third) {
+                int third_x = empty_area[third].first;
+                int third_y = empty_area[third].second;
+                v[third_x][third_y]=1;
 
-            const vector<pair<int, int>> adjacency = {
-                {0,1},{-1,0},{0,-1},{1,0}
-            };
-            for (auto d : adjacency) {
-                int x = cur.first+d.first;
-                int y = cur.second+d.second;
-
-                if (x<1 || x>n) {
-                    continue;
-                }
-                if (y<1 || y>n) {
-                    continue;
-                }
-                if (dist[x][y]>=0 || v[x][y]>lv) {
-                    continue;
-                }
-                q.push({x,y});
-                dist[x][y]=dist[cur.first][cur.second]+1;
-
-                if (!v[x][y] || v[x][y]>=lv) {
-                    continue;
-                }
-                if (min_dist>dist[x][y]) {
-                    d_x=x;
-                    d_y=y;
-                    min_dist=dist[x][y];
-                }
-                else if (min_dist==dist[x][y]) {
-                    // 가장 위쪽
-                    if (d_x>x) { 
-                        d_x=x;
-                        d_y=y;
-                    }
-                    // 가장 위쪽에 여러마리가 있다면, 가장 왼쪽
-                    else if (d_x==x && d_y>y) {
-                        d_x=x;
-                        d_y=y;
+                fill(is_visited.begin(),is_visited.end(),vector<bool>(m+1,false));
+                queue<pair<int, int>> q;
+                for (int i = 1; i<=n; ++i) {
+                    for (int j = 1; j<=m; ++j) {
+                        if (v[i][j]==0 || v[i][j]==1) {
+                            continue;
+                        }
+                        q.push({i,j});
+                        is_visited[i][j]=true;
                     }
                 }
+
+                while (!q.empty()) {
+                    auto cur = q.front();
+                    q.pop();
+
+                    const vector<pair<int, int>> adjacency = {
+                        {0,1},{-1,0},{0,-1},{1,0}
+                    };
+                    for (auto d : adjacency) {
+                        int x = cur.first+d.first;
+                        int y = cur.second+d.second;
+
+                        if (x<1 || x>n) {
+                            continue;
+                        }
+                        if (y<1 || y>m) {
+                            continue;
+                        }
+                        if (is_visited[x][y]) {
+                            continue;
+                        }
+                        if (v[x][y]==1) {
+                            continue;
+                        }
+                        is_visited[x][y]=true;
+                        q.push({x,y});
+                    }
+                }
+
+                int cnt = 0;
+                for (int i = 1; i<=n; ++i) {
+                    for (int j = 1; j<=m; ++j) {
+                        if (is_visited[i][j]) {
+                            ++cnt;
+                        }
+                    }
+                }
+                res=min(res,cnt);
+                v[third_x][third_y]=0;
             }
+            v[second_x][second_y]=0;
         }
-        if (d_x==-1 && d_y==-1) {
-            break;
-        }
-        cnt+=dist[d_x][d_y];
-        v[d_x][d_y]=0;
-        s_x=d_x;
-        s_y=d_y;
-
-        --lv_cnt;
-        if (!lv_cnt) {
-            ++lv;
-            lv_cnt=lv;
-        }
+        v[first_x][first_y]=0;
     }
-    cout << cnt;
+    cout << n*m-(wall_size+3)-res; // 0<->1+2
 
     return 0;
 }
