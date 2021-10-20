53dd8387327e213f37702585c9cfcf9accbeab4d
RyanJeong <wat6988@gmail.com>
2021-10-20 11:40:41 +0900

    Updated on Oct 20, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,25 +7,5 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, p, h;
-    cin>>n>>p>>h;
-
-    for (int i = 0; i<n; i++) {
-        int dollar;
-        cin>>dollar;
-
-        if (dollar>h) {
-            cout << "BBTV: Dollar reached " << dollar << " Oshloobs, A record!\n";
-            h=dollar;
-            p=dollar;
-            continue;
-        }
-
-        if (dollar<p) {
-            cout << "NTV: Dollar dropped by " << p-dollar << " Oshloobs\n";
-        }
-        p=dollar;
-    }
-
     return 0;
 }
