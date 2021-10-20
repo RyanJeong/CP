cb338271a73c65af68290150bd2307dee5cc1ac9
RyanJeong <wat6988@gmail.com>
2021-10-20 12:11:21 +0900

    Updated on Oct 20, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,5 +7,19 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    while (true) {
+        int n;
+        cin>>n;
+        if (!n) {
+            break;
+        }
+        for (int i = 1; i<=n; ++i) {
+            for (int j = 1; j<=i; ++j) {
+                cout << '*';
+            }
+            cout << '\n';
+        }
+    }
+
     return 0;
 }
