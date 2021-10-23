dcbe9bda2ecb7461523902ce970ffbeda784d651
RyanJeong <wat6988@gmail.com>
2021-10-23 15:01:27 +0900

    Updated on Oct 23, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,10 +7,29 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string str;
-    cin>>str;
-    int len = str.size();
-    cout << str.substr(0,len/2) << ' ' << str.substr(len/2);
+    int n;
+    cin>>n;
+    pair<int, string> res;
+    int max_cnt = -1;
+    while (n--) {
+        string str;
+        cin>>str;
+        int a, b;
+        cin>>a>>b;
+
+        int cnt = 0;
+        while (b>=a) {
+            b-=a;
+            b+=2;
+            ++cnt;
+            ++res.first;
+        }
+        if (max_cnt<cnt) {
+            max_cnt=cnt;
+            res.second=str;
+        }
+    }
+    cout << res.first << '\n' << res.second;
 
     return 0;
 }
