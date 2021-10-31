0975619b51a388e8736a60daebf5c1000d33b493
RyanJeong <wat6988@gmail.com>
2021-10-31 11:43:11 +0900

    Updated on Oct 31, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin>>n;
-    pair<int, string> res;
-    int max_cnt = -1;
-    while (n--) {
-        string str;
-        cin>>str;
-        int a, b;
-        cin>>a>>b;
-
-        int cnt = 0;
-        while (b>=a) {
-            b-=a;
-            b+=2;
-            ++cnt;
-            ++res.first;
-        }
-        if (max_cnt<cnt) {
-            max_cnt=cnt;
-            res.second=str;
-        }
-    }
-    cout << res.first << '\n' << res.second;
+int main() {
+    long long R, r;
+    std::cin >> R >> r;
+    std::cout << R * (R - 2 * r);
 
     return 0;
 }
