facc96e8403f42900244f43b3200144480c55059
RyanJeong <wat6988@gmail.com>
2021-11-01 19:06:43 +0900

    Updated on Nov 1, 2021

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,41 +1,18 @@
+/*
+  Copyright 2021 Ryan M. Jeong <ryan.m.jeong@hotmail.com>
+*/
 #include <iostream>
-#include <string>
-#include <algorithm>
 
 int main() {
-<<<<<<< HEAD
-    long long R, r;
-    std::cin >> R >> r;
-    std::cout << R * (R - 2 * r);
-
-    return 0;
-}
-=======
-  while (true) {
-    std::string s;
-    std::cin >> s;
-    if (s == "#") {
-      break;
+  std::string s;
+  std::cin >> s;
+  int n = s.size();
+  for (int i = 0; i < n; ++i) {
+    if (i > 0 && (n - i) % 3 == 0) {
+      std::cout << ',';
     }
-    std::string res;
-    std::reverse(s.begin(), s.end());
-    for (auto c : s) {
-      if (c == 'b') {
-        res.push_back('d');
-      } else if (c == 'd') {
-        res.push_back('b');
-      } else if (c == 'p') {
-        res.push_back('q');
-      } else if (c == 'q') {
-        res.push_back('p');
-      } else if (c == 'i' || c == 'o' || c == 'v' || c == 'w' || c == 'x') {
-        res.push_back(c);
-      } else {
-        res = "INVALID";
-        break;
-      }
-    }
-    std::cout << res << std::endl;
+    std::cout << s[i];
   }
+
+  return 0;
 }
->>>>>>> ef1546533ae7057b391b3d6df6ffa2e0ae48082d
