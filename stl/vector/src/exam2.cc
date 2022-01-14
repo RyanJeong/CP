c183d408e523291f826d2ae84e145deaded28de7
RyanJeong <wat6988@gmail.com>
2022-01-14 23:52:58 +0900

    Updated on Jan 14, 2022

--- stl/vector/src/exam2.cc
+++ stl/vector/src/exam2.cc
@@ -1,31 +1,13 @@
-* [STL - Container Classes:](/stl/)
-  * Sequence Container
-    * [Vector](/stl/vector/)
-    * [List](/stl/list/)
-    * [Double-ended Queue(Deque)](/stl/deque/)
-  * Container Adaptor
-    * [Stack](/stl/stack/)
-    * [Queue](/stl/queue/)
-    * [Priority Queue](/stl/priority_queue_heap/)
-  * Associative Container
-    * [Set](/stl/set/)
-    * [Map](/stl/map/)
-# Vector
-* 추천 문제
-  * [[BOJ] Strfry](https://www.acmicpc.net/problem/11328) [(소스코드)](./src/strfry.cc)
-  * [[BOJ] 최대 GCD](https://www.acmicpc.net/problem/9417) [(소스코드)](./src/max_gcd.cc) - C++ 컨테이너 조작 방법을 연습하기에 좋은 문제
-    * `cin.ignore`, `getline`
----
+/*
+  Copyright 2022 Ryan M. Jeong <ryan.m.jeong@hotmail.com>
+*/
 
-## [벡터(vector)](https://cplusplus.com/reference/vector/vector/)
-* STL 컨테이너 중 하나
-* 요소들을 순차적으로 보관하는 sequence container
-* 벡터는 배열과 비슷하지만, 배열(static 배열에 한함)은 크기가 불변인 반면 벡터는 크기가 가변
-* 벡터는 메모리 상에 요소들이 연속적으로 할당
+// CP
+#define CP do {                     \
+  std::ios::sync_with_stdio(false); \
+  std::cin.tie(NULL);               \
+} while (0)
 
-## `(constructor)`, `operator=`
-### [소스코드](./src/exam1.cc)
-```c++
 #include <iostream>
 #include <vector>
 
@@ -38,99 +20,8 @@ using std::getline;
 using std::vector;
 
 int main() {
-  // constructing vectors
-  // constructors used in the same order as described above:
-  vector<int> first;  // an empty vector
-  vector<int> second(4, 100);  // a vector of size 4 with all values as 100
-  vector<int> third(second.begin(), second.end());  // iterating through second
-  vector<int> fourth(third);  // use copy constructor
+  CP;
 
-  // the iterator constructor can also be used to construct from arrays:
-  int myints[] = { 16, 2, 77, 29 };
-  vector<int> fifth(myints, myints + sizeof myints / sizeof(int));
-
-  cout << "The contents of fifth are:";
-  for (auto it = fifth.begin(); it != fifth.end(); ++it) {
-    // The contents of fifth are: 16 2 77 29
-    cout << ' ' << *it;
-  }
-  cout << '\n';
-
-  // vector assignment
-  vector<int> foo(3, 0);
-  vector<int> bar = foo;
-  foo = vector<int>();
-  // foo's size: 0, foo's cap.: 0
-  cout << "foo's size: " << foo.size()
-       << ", foo's cap.: " << foo.capacity() << '\n';
-  // bar's size: 3, bar's cap.: 3
-  cout << "bar's size: " << bar.size()
-       << ", bar's cap.: " << bar.capacity() << '\n';
-
-  return 0;
-}
-
-```
----
-
-## `push_back`
-* 벡터의 마지막 요소 뒤에 새로운 요소를 추가하며, 시간복잡도는 <b>amortized O(1)</b>
-  * 벡터의 저장 공간에 여유가 있을 때, 새로운 요소 추가 시 시간복잡도는 <b>O(1)</b>
-  * 벡터의 요소 개수를 미리 알고 있다면, `reserve` 함수를 사용해 `push_back`의 시간복잡도를 <b>O(1)</b>로 만들 수 있음
-* 벡터의 저장 공간이 꽉 찬 상태에서 `push_back` 연산을 수행하면, <i>capacity</i>가 현재 <i>capacity</i>의 두 배가 됨
-
-## `pop_back`
-* 벡터의 마지막 요소를 제거하며, <b><i>capacity</i>는 그대로 보존</b>
-
-## `front`
-* 벡터의 첫 번째 요소 값 반환
-* <b>Calling this function on an empty container causes undefined behavior.</b>
-
-## `back`
-* 벡터의 마지막 요소 값 반환
-* <b>Calling this function on an empty container causes undefined behavior.</b>
-
-## `begin`
-* 벡터의 시작 요소를 <i>iterator</i> 타입으로 반환
-
-## `end`
-* 벡터의 마지막 요소의 다음 요소 (범위 밖 요소)를 <i>iterator</i> 타입으로 반환
-
-## `rbegin`
-* 벡터의 마지막 요소를 <i>iterator</i> 타입으로 반환
-
-## `rend`
-* 벡터의 시작 요소의 이전 요소 (범위 밖 요소)를 <i>iterator</i> 타입으로 반환
-
-## `insert`
-* <i>iterator</i> 타입이 가리키는 위치에 값 삽입
-
-## `erase`
-* <i>iterator</i> 타입이 가리키는 위치에 존재하는 값 삭제
-* 반환값은 <i>iterator</i>가 가리키고 있는 위치에서 <b>바로 뒤따르는 다음 요소 위치</b>를 <i>iterator</i> 타입으로 반환
-* 벡터의 값이 저장되는 배열의 시작 위치는 항상 <b>고정</b>
-  * `begin()` 값은 고정, `end()` 값이 바뀜 
-
-## `swap`
-* 두 벡터를 교환
-
-## `clear`
-* 벡터에 저장된 요소들을 비우되, <b><i>capacity</i>는 그대로 보존</b>
-
-### [소스코드](./src/exam2.cc)
-```c++
-#include <iostream>
-#include <vector>
-
-// iostream
-using std::cin;
-using std::cout;
-using std::getline;
-
-// vector
-using std::vector;
-
-int main() {
   // v1
   vector<int> v1(3, 5);
   cout << v1.size() << ' ' << v1.capacity() << '\n';  // 3 3
@@ -273,8 +164,3 @@ int main() {
 
   return 0;
 }
-```
-
----
-|[이전 - Array](/array/)|[목록](https://github.com/RyanJeong/CP#index)|[다음 - BFS & DFS](/bfs_dfs/)|
-|-|-|-|
