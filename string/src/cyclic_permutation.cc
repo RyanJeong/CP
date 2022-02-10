593cecdaca1c54f148a58d96390e888bdf4b101d
RyanJeong <wat6988@gmail.com>
2022-02-11 03:23:58 +0900

    Updated on Feb 11, 2022

---
+++ string/src/cyclic_permutation.cc
@@ -0,0 +1,78 @@
+/*
+  Copyright 2022 Ryan M. Jeong <ryan.m.jeong@hotmail.com>
+*/
+
+// CP
+#define CP do {                     \
+  std::ios::sync_with_stdio(false); \
+  std::cin.tie(NULL);               \
+} while (0)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// iostream
+using std::cin;
+using std::cout;
+
+// vector
+using std::vector;
+
+// string
+using std::string;
+
+// algorithm
+using std::rotate;
+
+template <typename T>
+vector<int> GetFail(const T& pattern);
+template <typename T>
+vector<int> Kmp(const T& str, const T& pattern);
+
+int main() {
+  CP;
+
+  string str, pattern;
+  cin >> str >> pattern;
+  str.insert(str.end(), str.begin(), str.end() - 1);
+  cout << Kmp(str, pattern).size();
+
+  return 0;
+}
+
+template <typename T>
+vector<int> GetFail(const T& p) {
+  vector<int> fail(p.size());
+  int j = 0;
+  for (int i = 1; i < p.size(); i++) {
+    while (j > 0 && p[i] != p[j])
+      j = fail[j-1];
+    if (p[i] == p[j])
+      fail[i] = ++j;
+  }
+
+  return fail;
+}
+
+template <typename T>
+vector<int> Kmp(const T& s, const T& p) {
+  vector<int> fail = GetFail(p);
+  vector<int> ans;
+  int j = 0;
+  for (int i = 0; i < s.size(); i++) {
+    while (j > 0 && s[i] != p[j])
+      j = fail[j-1];
+    if (s[i] == p[j]) {
+      if (j == p.size() - 1) {
+        ans.push_back(i - p.size() + 1);
+        j = fail[j];
+      } else {
+        j++;
+      }
+    }
+  }
+
+  return ans;
+}
