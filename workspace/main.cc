d89b49964c5184dc94cff573248f85db12ffcc9f
RyanJeong <wat6988@gmail.com>
2022-06-04 21:19:15 +0900

    Updated on Jun 4, 2022

--- workspace/main.cc
+++ workspace/main.cc
@@ -9,54 +9,56 @@
 } while (0)
 
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <cmath>
-#include <algorithm>
+#include <functional>
+#include <utility>
 
+// iostream
 using std::cin;
 using std::cout;
 
+// queue
+using std::priority_queue;
+
+// vector
 using std::vector;
 
-using std::sqrt;
+// functional
+using std::less;
 
-using std::lower_bound;
-
-template<typename T>
-vector<int> eratosthenes_sieve(T from, T to) {
-  static vector<bool> is_composition(to + 1);  // 1-based
-  vector<int> primes;
-
-  if (from < 2)  // 1 is not a prime number
-    from = 2;
-  for (T i = 2; i * i <= to; ++i) {
-    if (is_composition[i])
-      continue;
-    for (T j = i * i; j <= to; j += i)
-      is_composition[j] = true;
-  }
-  for (T i = from; i <= to; ++i) {
-    if (!is_composition[i])
-      primes.push_back(i);
-  }
-
-  return primes;
-}
+// functional
+using std::pair;
 
 int main() {
   CP;
 
-  int from = 0;
-  int to = static_cast<int>(sqrt(4e9));
-  vector<int> primes = eratosthenes_sieve(from, to);
-  int64_t t;
-  cin >> t;
-  while (t--) {
-    int64_t n;
-    cin >> n;
-    auto it = lower_bound(primes.begin(), primes.end(), n);
-    cout << *it << '\n';
+  int n;
+  cin >> n;
+  priority_queue<
+      pair<int, int>, vector<pair<int, int>>, less<pair<int, int>>> max_heap;
+  while (n--) {
+    int m;
+    cin >> m;
+    int max = 0;
+    for (int i = 0; i < m; ++i) {
+      int temp;
+      cin >> temp;
+      if (max < temp)
+        max = temp;
+    }
+    max_heap.push({max, m});
   }
+
+  auto curr = max_heap.top();
+  max_heap.pop();
+  int64_t res = 0;
+  while (!max_heap.empty()) {
+    auto temp = max_heap.top();
+    max_heap.pop();
+    res += static_cast<int64_t>(curr.first - temp.first) * temp.second;
+  }
+  cout << res;
 
   return 0;
 }
